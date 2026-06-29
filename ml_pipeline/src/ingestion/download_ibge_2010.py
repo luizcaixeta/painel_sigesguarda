bd46f76a3f701969188b8ee315f3a989de79343d
luizcaixeta <luizfernandosusu@gmail.com>
2026-06-29 07:37:12 -0300

    feat(download_ibge2010): add directory organization to download_ibge_2010.py

--- ml_pipeline/src/ingestion/download_ibge_2010.py
+++ ml_pipeline/src/ingestion/download_ibge_2010.py
@@ -1,4 +1,5 @@
 from pathlib import Path 
+import shutil
 import requests 
 import zipfile 
 
@@ -7,6 +8,15 @@ RAW_DIR = PROJETCT_ROOT / "data" / "ibge2010" / "raw"
 
 LINKS = {
     "censo2010_pr": "https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/PR_20260615.zip"
+}
+
+REQUIRED_IBGE_2010_FILES = {
+    "Basico_PR.csv",
+    "PessoaRenda_PR.csv",
+    "ResponsavelRenda_PR.csv",
+    "Pessoa01_PR.csv",
+    "Pessoa13_PR.csv",
+    "Domicilio01_PR.csv"
 }
 
 def download_ibge_2010_data(links: dict[str, str]) -> None:
@@ -41,9 +51,38 @@ def unzip_ibge_2010_data() -> None:
 
         zip_path.unlink()
 
+def move_data_ibge_2010_files() -> None:
+    base_dir = RAW_DIR / "censo2010_pr" / "Base informaçoes setores2010 universo PR"
+    csv_dir = base_dir / "CSV"
+
+    for file_path in csv_dir.glob("*.csv"):
+        destination = RAW_DIR / file_path.name
+        file_path.rename(destination)
+
+def delete_ibge_2010_extracted_folder() -> None:
+    extracted_dir = RAW_DIR / "censo2010_pr"
+
+    shutil.rmtree(extracted_dir)
+
+def delete_unused_ibge_2010_files() -> None:
+    for file_path in RAW_DIR.iterdir():
+        if not file_path.is_file():
+            continue 
+
+        if file_path.name in REQUIRED_IBGE_2010_FILES:
+            continue 
+
+        if file_path.name == ".gitkeep":
+            continue 
+
+        file_path.unlink()
+
 def main() -> None:
     download_ibge_2010_data(LINKS)
     unzip_ibge_2010_data()
+    move_data_ibge_2010_files()
+    delete_ibge_2010_extracted_folder()
+    delete_unused_ibge_2010_files()
 
 if __name__ == "__main__":
     main()
