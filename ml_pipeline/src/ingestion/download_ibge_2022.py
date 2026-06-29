38c457090a0fdad5bb40664a6f6a5a935d7fb6f5
luizcaixeta <luizfernandosusu@gmail.com>
2026-06-29 07:37:48 -0300

    feat(download_ibge2022): add directory organization to download_ibge_2022.py

--- ml_pipeline/src/ingestion/download_ibge_2022.py
+++ ml_pipeline/src/ingestion/download_ibge_2022.py
@@ -1,4 +1,5 @@
 from pathlib import Path 
+import shutil
 import requests 
 import zipfile 
 
@@ -11,6 +12,14 @@ LINKS = {
     "agregados_bairros_domicilio1": "https://ftp.ibge.gov.br/Censos/Censo_Demografico_2022/Agregados_por_Setores_Censitarios/Agregados_por_Bairro_csv/Agregados_por_bairros_caracteristicas_domicilio1_BR.zip",
     "agregados_bairros_domicilio2": "https://ftp.ibge.gov.br/Censos/Censo_Demografico_2022/Agregados_por_Setores_Censitarios/Agregados_por_Bairro_csv/Agregados_por_bairros_caracteristicas_domicilio2_BR_20250417.zip",
     "agregados_bairros_renda_responsavel": "https://ftp.ibge.gov.br/Censos/Censo_Demografico_2022/Agregados_por_Setores_Censitarios_Rendimento_do_Responsavel/Agregados_por_bairros_renda_responsavel_BR_20260508_csv.zip"
+}
+
+REQUIRED_IBGE_2022_FILES = {
+    "Agregados_por_bairros_basico_BR.csv",
+    "Agregados_por_bairros_alfabetizacao_BR.csv",
+    "Agregados_por_bairros_renda_responsavel_BR.csv",
+    "Agregados_por_bairros_caracteristicas_domicilio1_BR.csv",
+    "Agregados_por_bairros_caracteristicas_domicilio2_BR.csv",
 }
 
 def download_ibge_2022_data(links: dict[str, str]) -> None:
@@ -45,9 +54,41 @@ def unzip_ibge_2022_data() -> None:
 
         zip_path.unlink()
 
+def move_data_ibge_2022_files() -> None:
+    for file_path in RAW_DIR.rglob("*.csv"):
+        if file_path.parent == RAW_DIR:
+            continue 
+
+        if file_path.name not in REQUIRED_IBGE_2022_FILES:
+            continue 
+
+        destination = RAW_DIR / file_path.name 
+        file_path.rename(destination)
+
+def delete_ibge_2022_extracted_folders() -> None:
+    for path in RAW_DIR.iterdir():
+        if path.is_dir():
+            shutil.rmtree(path)
+
+def delete_unused_ibge_2022_files() -> None:
+    for file_path in RAW_DIR.iterdir():
+        if not file_path.is_file():
+            continue 
+
+        if file_path.name in REQUIRED_IBGE_2022_FILES:
+            continue 
+
+        if file_path.name == ".gitkeep":
+            continue 
+
+        file_path.unlink()
+
 def main() -> None:
     download_ibge_2022_data(LINKS)
     unzip_ibge_2022_data()
+    move_data_ibge_2022_files()
+    delete_ibge_2022_extracted_folders()
+    delete_unused_ibge_2022_files()
 
 if __name__ == "__main__":
     main()
