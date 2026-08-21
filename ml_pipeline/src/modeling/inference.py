36c5f80ece2260e96b6bbef3fe742eb0a41fdef6
luizcaixeta <luizfernandosusu@gmail.com>
2026-08-21 17:09:20 -0300

    fix(modeling): update load_sql_into_dataframe

--- ml_pipeline/src/modeling/inference.py
+++ ml_pipeline/src/modeling/inference.py
@@ -1,6 +1,6 @@
 # %%
 import mlflow.pyfunc 
-from modeling.data import db_url, load_gold_dataframe
+from modeling.data import SQL_DIR, read_sql, load_sql_into_dataframe
 from modeling.config import FEATURES
 
 from sklearn.pipeline import Pipeline
@@ -9,9 +9,11 @@ import pandas as pd
 
 from modeling.config import FEATURES, TARGET 
 
+GOLD_QUERY = read_sql(SQL_DIR, 'load_gold_data', 'gold_query.sql')
+
 model = mlflow.pyfunc.load_model("models:/m-8a52ff93a1144b738a3df621d861947a")
 
-df = load_gold_dataframe(db_url)
+df = load_sql_into_dataframe(GOLD_QUERY)
 
 X = df.loc[:, FEATURES].head(250)
 
