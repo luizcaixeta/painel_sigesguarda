f4530614d3d22ec971bb56542578459df1c18b0f
luizcaixeta <luizfernandosusu@gmail.com>
2026-09-09 18:12:48 -0300

    add ocorrencias struct

---
+++ api/internal/domain/ocorrencia.go
@@ -0,0 +1,23 @@
+package domain
+
+import "time"
+
+type Ocorrencia struct {
+	BairroID   string
+	Categoria  string
+	Mes        time.Time
+	Quantidade int32
+}
+
+type OcorrenciasFilter struct {
+	BairroIDs  []string
+	Categorias []string
+	Meses      []time.Time
+	De         *time.Time
+	Ate        *time.Time
+}
+
+type CurrentBatch struct {
+	ID          string
+	DataThrough time.Time
+}
