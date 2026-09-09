41c32794d984b6f1b4aca86c5feab409e60bfa72
luizcaixeta <luizfernandosusu@gmail.com>
2026-09-09 17:39:12 -0300

    add bairro geojson struct

--- api/internal/domain/bairro.go
+++ api/internal/domain/bairro.go
@@ -1,6 +1,14 @@
 package domain
 
+import "encoding/json"
+
 type Bairro struct {
 	ID   string
 	Nome string
+}
+
+type BairroGeometry struct {
+	ID       string
+	Nome     string
+	Geometry json.RawMessage
 }
