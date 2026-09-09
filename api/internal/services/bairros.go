ecfde8c98c8e57e7e356b7bc545f1a0c219b25dd
luizcaixeta <luizfernandosusu@gmail.com>
2026-09-09 15:52:25 -0300

    feat(services): add bairros services

---
+++ api/internal/services/bairros.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/luizcaixeta/painel_sigesguarda/api/internal/domain"
+)
+
+var ErrDataNotReady = errors.New("data not ready")
+
+type BairroRepository interface {
+	ListBairros(context.Context) ([]domain.Bairro, error)
+}
+
+type BairroService struct {
+	repository BairroRepository
+}
+
+func NewBairroService(repository BairroRepository) *BairroService {
+	return &BairroService{repository: repository}
+}
+
+func (service *BairroService) ListBairros(
+	ctx context.Context,
+) ([]domain.Bairro, error) {
+	bairros, err := service.repository.ListBairros(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("list bairros: %w", err)
+	}
+
+	if len(bairros) != 75 {
+		return nil, ErrDataNotReady
+	}
+
+	return bairros, nil
+}
