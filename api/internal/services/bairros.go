f5f265e5412b354b31666ee59fdc17dafef62ca7
luizcaixeta <luizfernandosusu@gmail.com>
2026-09-09 17:44:15 -0300

    eat(services): add bairros geojson services

--- api/internal/services/bairros.go
+++ api/internal/services/bairros.go
@@ -12,6 +12,7 @@ var ErrDataNotReady = errors.New("data not ready")
 
 type BairroRepository interface {
 	ListBairros(context.Context) ([]domain.Bairro, error)
+	ListBairrosGeoJSON(context.Context) ([]domain.BairroGeometry, error)
 }
 
 type BairroService struct {
@@ -22,12 +23,23 @@ func NewBairroService(repository BairroRepository) *BairroService {
 	return &BairroService{repository: repository}
 }
 
-func (service *BairroService) ListBairros(
-	ctx context.Context,
-) ([]domain.Bairro, error) {
+func (service *BairroService) ListBairros(ctx context.Context) ([]domain.Bairro, error) {
 	bairros, err := service.repository.ListBairros(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("list bairros: %w", err)
+	}
+
+	if len(bairros) != 75 {
+		return nil, ErrDataNotReady
+	}
+
+	return bairros, nil
+}
+
+func (service *BairroService) ListBairrosGeoJSON(ctx context.Context) ([]domain.BairroGeometry, error) {
+	bairros, err := service.repository.ListBairrosGeoJSON(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("list bairro geometries: %w", err)
 	}
 
 	if len(bairros) != 75 {
