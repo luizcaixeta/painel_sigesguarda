d15e96b2a9c8714965425c1c2b4f625b8c2d711c
luizcaixeta <luizfernandosusu@gmail.com>
2026-09-09 11:45:44 -0300

    feat(api) readiness checker

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -2,14 +2,22 @@ package router
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/luizcaixeta/painel_sigesguarda/api/internal/handler"
 )
 
-func New() http.Handler {
+func New(
+	readinessChecker handler.ReadinessChecker,
+	queryTimeout time.Duration,
+) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", handler.HealthCheckHandler)
+	mux.Handle(
+		"GET /readyz",
+		handler.NewReadinessHandler(readinessChecker, queryTimeout),
+	)
 
 	return mux
 }
